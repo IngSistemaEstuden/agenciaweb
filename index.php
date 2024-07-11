ff1a2df6a63f73a1bba2738b57715eea70dcd29f
IngSistemaEstuden <9vo.semestre@gmail.com>
2024-07-11 13:52:04 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agencia de trabajo web</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="css/estiloIndex.css">
+</head>
+<body>
+    <header>
+        <div class="container hero">
+            <h1>TRABAJOS YAPACANI</h1>
+            <div class="dropdown login-dropdown ml-auto">
+                <button class="btn btn-link dropdown-toggle" srl="button" id="loginDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="img/icono.png" width="50px">
+                </button>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="loginDropdown">
+                    <a class="dropdown-item" href="php/InicioSesion.php">Iniciar Sesión</a>
+                    <a class="dropdown-item" href="php/InicioSesion.php">Registrarse</a>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section class="banner">
+        <div class="content-banner">
+            <p>BOLIVIA CHICA</p>
+            <h2>AGENCIA VIRTUAL DE EMPLEO</h2>
+            <h6>CONECTANDO TALENTOS CON OPORTUNIDADES</h6>
+        </div>
+    </section>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="/">MENU</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a class="nav-link" href="#">SOBRE NOSOTROS</a></li>
+                    <li class="nav-item"><a class="nav-link" href="#">CONTACTOS</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main class="main-content">
+        <section class="container top-categories">
+            <h1 class="heading-1">CATEGORIAS</h1>
+            <div id="categoryCarousel1" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="container-categories">
+                            <div class="card-category category-Albañil">
+                                <p>Albañil</p>
+                                <a href="https://es.wiktionary.org/wiki/alba%C3%B1il#:~:text=Persona%20cuyo%20oficio%20es%20el,la%20escuadra%20y%20el%20nivel.">Ver más</a>
+                            </div>
+                            <div class="card-category category-AtencionCliente">
+                                <p>Atención al cliente</p>
+                                <a href="https://es.wiktionary.org/wiki/alba%C3%B1il#:~:text=Persona%20cuyo%20oficio%20es%20el,la%20escuadra%20y%20el%20nivel.">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="container-categories">
+                            <div class="card-category category-Electrico">
+                                <p>Eléctrico</p>
+                                <a href="https://es.wiktionary.org/wiki/alba%C3%B1il#:~:text=Persona%20cuyo%20oficio%20es%20el,la%20escuadra%20y%20el%20nivel.">Ver más</a>
+                            </div>
+                            <div class="card-category category-TrabajoCampo">
+                                <p>Trabajo de Campo</p>
+                                <a href="https://es.wiktionary.org/wiki/alba%C3%B1il#:~:text=Persona%20cuyo%20oficio%20es%20el,la%20escuadra%20y%20el%20nivel.">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#categoryCarousel1" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#categoryCarousel1" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </section>
+        
+        <section class="container top-categories">
+            <div id="categoryCarousel2" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <div class="container-categories">
+                            <div class="card-category category-Salud">
+                                <p>Salud</p>
+                                <a href="https://es.wiktionary.org/wiki/alba%C3%B1il#:~:text=Persona%20cuyo%20oficio%20es%20el,la%20escuadra%20y%20el%20nivel.">Ver más</a>
+                            </div>
+                            <div class="card-category category-Tecnologia">
+                                <p>Tecnología</p>
+                                <a href="https://drive.google.com/file/d/1AfqzVby0A9Z-dVzTFOScKB1-inarLWcI/view?usp=drive_link">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="container-categories">
+                            <div class="card-category category-Diseno">
+                                <p>Diseño</p>
+                                <a href="https://es.wiktionary.org/wiki/alba%C3%B1il#:~:text=Persona%20cuyo%20oficio%20es%20el,la%20escuadra%20y%20el%20nivel.">Ver más</a>
+                            </div>
+                            <div class="card-category category-Ventas">
+                                <p>Ventas</p>
+                                <a href="https://es.wiktionary.org/wiki/alba%C3%B1il#:~:text=Persona%20cuyo%20oficio%20es%20el,la%20escuadra%20y%20el%20nivel.">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#categoryCarousel2" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#categoryCarousel2" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </section>
+    </main>
+  
+
+    <section class="blog-content">
+        <h5>DESARROLLADO POR:</h5>
+        <span>22 Junio 2024</span>
+        <p>Somos estudiantes de Ingeniería en Sistemas de la Universidad Gabriel René Moreno, en la Facultad Integral de Ichilo. Estamos desarrollando esta plataforma web con el objetivo de conectar talentos con oportunidades laborales en Yapacaní.</p>
+        <p>Nuestra misión es facilitar el proceso de búsqueda de empleo tanto para los candidatos como para las empresas, creando un espacio virtual donde puedan encontrarse y colaborar.</p>
+        <p>Este proyecto es especialmente relevante para nuestra comunidad, ya que Yapacaní tiene un gran potencial de crecimiento y desarrollo. Creemos firmemente en el poder de la tecnología para mejorar la empleabilidad y la accesibilidad a trabajos dignos, lo que es crucial para el progreso local.</p>
+        <p>Con esta iniciativa, aspiramos a contribuir al desarrollo económico y social de nuestra región, brindando herramientas modernas y eficientes para que todos puedan acceder a mejores oportunidades laborales.</p>
+    </section>
+
+    <footer class="bg-dark text-white mt-4 p-4 text-center">
+        <p>Contacto: info@yapacaniempleos.bo | Tel: +591 63383676</p>
+    </footer>
+
+    <!-- jQuery (asegúrate de incluirlo antes de Bootstrap) -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <!-- Popper.js (necesario para Bootstrap) -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
